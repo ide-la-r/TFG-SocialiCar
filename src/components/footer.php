1f155e0275266f0a0f176cda6b8f5da919fe9856
SrPirson <fcp0009@alu.medac.com>
2025-04-23 19:55:06 +0200

    Arreglar fallo en navbar y footer

--- src/components/footer.php
+++ src/components/footer.php
@@ -128,4 +128,6 @@
   </div>
   <!-- Grid container -->
 </footer>
-<!-- Footer -->
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
