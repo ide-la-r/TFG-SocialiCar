a274e6dc033894f06d6a8d112aaf100c938e3744
SrPirson <fcp0009@alu.medac.com>
2025-05-08 00:39:08 +0200

    Creación del chat entre particulares con historial guardado en BBDD

--- src/components/links.php
+++ src/components/links.php
@@ -1,5 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 <link rel="stylesheet" href="/src/styles/global.css">
 <link rel="stylesheet" href="/src/styles/navbar.css">
 <link rel="stylesheet" href="/src/styles/footer.css">
