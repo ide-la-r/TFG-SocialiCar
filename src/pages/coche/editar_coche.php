aa7ff7f0f3d8368b57df7d98ba7a1b7986cba790
pma152402 <pma152402@gmail.com>
2025-05-18 01:38:47 +0200

    editar coche

--- src/pages/coche/editar_coche.php
+++ src/pages/coche/editar_coche.php
@@ -23,9 +23,73 @@
     ?>
 
     <style>
+        
         .error {
             color: #e03131;
             font-size: 1em;
+        }
+
+        body {
+            position: relative;
+            background: url('../../img/fondo_editar.jpg');
+            background-size: cover;
+            background-position: center;
+            min-height: 100vh;
+            margin: 0;
+        }
+
+        body::before {
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: linear-gradient(to right,
+
+                    rgba(73, 73, 73, 0.4) 0%,
+                    rgba(184, 232, 235, 0.3) 58%,
+                    rgba(114, 114, 114, 0.7) 100%);
+
+            z-index: 0;
+            pointer-events: none;
+        }
+
+
+        
+        .card {
+            border-radius: 30px;
+            box-shadow: 0 8px 18px rgba(0, 0, 0, 0.9);
+            margin: 1rem auto;
+            max-width: 800px;
+        }
+
+
+        h3 {
+            color: #222831;
+            font-weight: 700;
+            font-size: 2.4rem;
+            margin-bottom: 2rem;
+            border-left: 6px solid #00adb5;
+            padding-left: 1rem;
+            align-self: flex-start;
+            text-align: left;
+        }
+
+
+        @media (max-width: 576px) {
+            .col-md-3 {
+                flex: 0 0 100% !important;
+                max-width: 100% !important;
+            }
+        }
+
+
+        /* PONER EN LOS DEMAS ARCHIVOS */
+        .form-floating>.form-control,
+        .form-floating>.form-select {
+            border: 1px solid black;
+            border-radius: 20px;
         }
     </style>
 </head>
