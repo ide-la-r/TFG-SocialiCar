310c5472df125aed5456c1b628345dbbd34e8436
pma152402 <pma152402@gmail.com>
2025-05-18 13:31:52 +0200

    correccion 3

--- src/pages/rentacar/mostrar_coches.php
+++ src/pages/rentacar/mostrar_coches.php
@@ -210,6 +210,7 @@
                 </div>
             </div>
 
+            
             <!-- OTROS FILTROS -->
             <div class="mt-3">
                 <div class="d-flex justify-content-between align-items-center" data-bs-toggle="collapse" href="#equipamientoCollapse">
