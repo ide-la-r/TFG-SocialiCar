a322c4d66f52c7facaccebb8fe4e20950766b8f7
SrPirson <fcp0009@alu.medac.com>
2025-06-02 20:39:37 +0200

    Codigo de verificación por correo electronico

--- src/pages/coche/nuevo_coche.php
+++ src/pages/coche/nuevo_coche.php
@@ -397,9 +397,9 @@
                 $provincias_espana = [
                     "Álava", "Albacete", "Alicante", "Almería", "Asturias", "Ávila",
                     "Badajoz", "Barcelona", "Burgos", "Cáceres", "Cádiz", "Cantabria", "Castellón",
-                    "Ciudad Real", "Córdoba", "Cuenca", "Girona", "Granada", "Guadalajara", "Guipúzcoa",
-                    "Huelva", "Huesca", "Illes Balears", "Jaén", "La Coruña", "La Rioja", "Las Palmas",
-                    "León", "Lleida", "Lugo", "Madrid", "Málaga", "Murcia", "Navarra", "Ourense",
+                    "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Granada", "Guadalajara", "Guipúzcoa",
+                    "Huelva", "Huesca", "Islas Baleares", "Jaén", "La Coruña", "La Rioja", "Las Palmas",
+                    "León", "Lérida", "Lugo", "Madrid", "Málaga", "Murcia", "Navarra", "Orense",
                     "Palencia", "Pontevedra", "Salamanca", "Santa Cruz de Tenerife", "Segovia", "Sevilla",
                     "Soria", "Tarragona", "Teruel", "Toledo", "Valencia", "Valladolid", "Vizcaya",
                     "Zamora", "Zaragoza", "Ceuta", "Melilla"
---
+++ src/pages/usuario/validar_usuario.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SocialiCar - Easter Egg</title>
+    <?php include_once '../../components/links.php'; ?>
+    <link rel="icon" href="../../../src/img/favicon.png" />
+    <link rel="stylesheet" href="../../../src/styles/index.css">
+    <?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    require(__DIR__ . "/../../config/conexion.php");
+
+    session_start();
+    ?>
+    <style>
+        .error {
+            color: #e03131;
+            font-size: 1em;
+        }
+    </style>
+</head>
+<body>
+    <?php include_once '../../components/navbar.php'; ?>
+        
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $codigo_real = $_SESSION["usuario"]["codigo"];
+            $codigo_usuario = $_POST['codigo'];
+            
+            if (empty($codigo_usuario)) {
+                $err_codigo = "Debes introducir el codigo de verificación.";
+            } else {
+                if ($codigo_usuario != $codigo_real) {
+                    $err_codigo = "El código de verificación es incorrecto.";
+                } else{
+                    $codigo = $codigo_usuario;
+                }
+            }
+        }
+    ?>
+    <div class="container mt-5 pt-5 d-flex justify-content-center align-items-center">
+        <div class="card text-center card_registro w-100 p-4 rounded-4 shadow mx-auto" style="max-width: 420px;">
+            <h1 class="register pt-2 pb-3">Confirmar correo electrónico</h1>
+            <form action="" method="post">
+                <div class="row gy-3">
+
+                    <div class="mb-2 col-12">
+                        <div class="form-floating">
+                            <?php $correo_usuario = $_SESSION["usuario"]["correo"]; ?>
+                            <input class="form-control" type="email" id="correo" placeholder="Correo electrónico*" name="correo" value="<?php echo $correo_usuario; ?>" disabled>
+                            <label for="correo">Correo</label>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-2 col-12">
+                        <div class="form-floating">
+                            <input id="codigo" class="form-control <?php if (isset($err_codigo)) echo 'is-invalid'; ?>" type="text" placeholder="Código*" name="codigo" value="<?php if (isset($codigo_usuario)) echo $codigo_usuario; ?>">
+                            <label for="codigo">Código</label>
+                            <?php if (isset($err_codigo)) echo "<span class='error'>$err_codigo</span>"; ?>
+                        </div>
+
+                    </div>
+
+                    <div class="col-12 mt-3">
+                        <input type="submit" class="btn btn-primary w-100" value="Confirmar">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <?php include_once '../../components/footer-example.php'; ?>
+
+
+    <?php
+        if (isset($codigo)) {
+            // Supongamos que la identificación está guardada en la sesión
+            $identificacion = $_SESSION["usuario"]["identificacion"];
+        
+            $actualizar_usuario = $_conexion->prepare("UPDATE usuario SET verificado = 1 WHERE identificacion = ?");
+            $actualizar_usuario->bind_param("s", $identificacion);
+            $actualizar_usuario->execute();
+        
+            if ($actualizar_usuario->affected_rows > 0) {
+                
+                echo "<script>window.location.href = '/';</script>";
+                exit();
+            } else {
+                echo "<p class='error'>No se pudo actualizar el estado de verificación.</p>";
+            }
+        }
+    ?>
+</body>
+</html>
