9d96c18b39d9636ef119d67e5e3918b4e93c483b
SrPirson <fcp0009@alu.medac.com>
2025-06-02 22:17:43 +0200

    Corrección de error validar correo

--- src/pages/usuario/validar_usuario.php
+++ src/pages/usuario/validar_usuario.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SocialiCar - Easter Egg</title>
+    <title>SocialiCar - Comparte tu coche</title>
     <?php include_once '../../components/links.php'; ?>
     <link rel="icon" href="../../../src/img/favicon.png" />
     <link rel="stylesheet" href="../../../src/styles/index.css">
